cbe129ef6ea672803753dce0e43f9460efb539d1
Enzo Carmo <enzocarmo64@gmail.com>
2025-05-07 17:49:55 -0300

    Commit

--- src/components/ComparativeCellRenderer.vue
+++ src/components/ComparativeCellRenderer.vue
@@ -1,20 +1,22 @@
 <template>
     <div class="comparative-cell">
-        <span :class="{ 'negative': isNegative, 'positive': !isNegative && hasValue }">
+        <span :class="{ 'negative': isNegative, 'positive': isPositive, 'zero': isZero }">
             {{ formattedValue }}
             <PhCaretDown v-if="isNegative && hasValue" :size="20" weight="fill" class="icon" />
-            <PhCaretUp v-if="!isNegative && hasValue" :size="20" weight="fill" class="icon" />
+            <PhCaretUp v-if="isPositive && hasValue" :size="20" weight="fill" class="icon" />
+            <PhMinus v-if="isZero && hasValue" :size="20" weight="fill" class="icon" />
         </span>
     </div>
 </template>
 
 <script>
-import { PhCaretUp, PhCaretDown } from '@phosphor-icons/vue'
+import { PhCaretUp, PhCaretDown, PhMinus } from '@phosphor-icons/vue'
 
 export default {
     components: {
         PhCaretUp,
-        PhCaretDown
+        PhCaretDown,
+        PhMinus
     },
     name: "ComparativeCellRenderer",
     props: {
@@ -34,6 +36,20 @@ export default {
                 : this.params.value;
             return this.hasValue && value < 0;
         },
+        isZero() {
+            // Parse string or number values
+            const value = typeof this.params.value === 'string'
+                ? parseFloat(this.params.value)
+                : this.params.value;
+            return this.hasValue && value === 0;
+        },
+        isPositive() {
+            // Parse string or number values
+            const value = typeof this.params.value === 'string'
+                ? parseFloat(this.params.value)
+                : this.params.value;
+            return this.hasValue && value > 0;
+        },
         formattedValue() {
             return this.params.valueFormatted || this.params.value;
         }
@@ -46,19 +62,23 @@ export default {
     height: 100%;
     display: flex;
     align-items: center;
-
+    
     span {
         display: flex;
         align-items: center;
-
+        
         &.negative {
             color: #F8471C;
         }
-
+        
         &.positive {
             color: #27be52;
         }
-
+        
+        &.zero {
+            color: #140F10;
+        }
+        
         .icon {
             padding-left: 4px;
         }
